c810965d45efe1d7e798ae6d59d18d603fb72fab
Babich <nyutonyka@gmail.com>
2022-04-08 12:45:03 +0300

    Modified model client

--- src/models/Client.java
+++ src/models/Client.java
@@ -1,10 +1,15 @@
 package models;
 
-public class Client {
+import base.Discount;
+import base.Payment;
+
+public class Client implements Payment, Discount {
 
     private String name;
-
+    private int quantity;
+    private double price;
     private final static int DISCOUNT_RATE = 15;
+
 
     public String getName() {
         return name;
@@ -12,5 +17,35 @@ public class Client {
 
     public void setName(String name) {
         this.name = name;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public double calculatePayment(int quota, double price) {
+        return quota * price;
+    }
+
+    @Override
+    public double calculatePayment(double payment, double discount) {
+        return payment - discount;
+    }
+
+    @Override
+    public double calculateDiscount(double payment) {
+        return payment * DISCOUNT_RATE / 100;
     }
 }
