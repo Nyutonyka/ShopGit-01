f5d37350c152a535bad917c6fab1415d69dc7700
Babich <nyutonyka@gmail.com>
2022-04-08 11:53:10 +0300

    Add client controller model

---
+++ src/controllers/ClientController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import models.Client;
+
+public class ClientController {
+
+    Client model;
+
+    public ClientController(Client model) {
+        this.model = model;
+    }
+}
