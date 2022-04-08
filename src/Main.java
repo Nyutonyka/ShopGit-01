7015e9ce4bbdd8ace404fdf4f71cfde327d042ae
Babich <nyutonyka@gmail.com>
2022-04-08 12:51:46 +0300

    Add author and version

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
 
+    /**
+     * @author Babich Anna
+     * @version 1.0.0
+     */
+
     public static void main(String[] args) {
 
         System.out.println("New program: Store!");
     }
-
 }
